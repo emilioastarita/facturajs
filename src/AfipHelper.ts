77de75e280a5e37cc57905c437482d0f08f347eb
chulo <emilio.astarita@gmail.com>
2018-05-09 11:43:15 -0300

    FIX: Prod URLs.

--- src/AfipHelper.ts
+++ src/AfipHelper.ts
@@ -43,8 +43,8 @@ export class AfipHelper {
             login: 'https://wsaahomo.afip.gov.ar/ws/services/LoginCms?wsdl'
         },
         prod: {
-            service: 'https://wswhomo.afip.gov.ar/{name}/service.asmx?wsdl',
-            login: 'https://wsaahomo.afip.gov.ar/ws/services/LoginCms?wsdl'
+            service: 'https://servicios1.afip.gov.ar/{service}/service.asmx?WSDL',
+            login: 'https://wsaa.afip.gov.ar/ws/services/LoginCms?wsdl'
         },
     };
 
