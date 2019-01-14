0497db106f1f02362bebb68983fcdff851935291
Emilio Astarita <emilio.astarita@gmail.com>
2019-01-14 19:19:39 -0300

    Little refactor process.env parsing.

--- src/util.ts
+++ src/util.ts
@@ -2,7 +2,9 @@ import * as forge from "node-forge";
 import xml2js = require('xml2js');
 import util = require('util');
 import * as fs from "fs";
-const _debug = require('debug')('facturajs');
+const LOG_RUNTIME_LEVEL: LOG = parseProcessLevel();
+const LOG_NAMESPACE = 'facturajs';
+const _debug = require('debug')(LOG_NAMESPACE);
 
 export const readFile = util.promisify(fs.readFile);
 export const writeFile = util.promisify(fs.writeFile);
@@ -14,18 +16,24 @@ export enum LOG {
     INFO,
     DEBUG,
 }
-const currentLevel : LOG = typeof(process.env['LOG_LEVEL']) !== "undefined" ?  parseInt(<string>process.env.LOG_LEVEL, 10) : LOG.INFO;
-export function debug(level : number, ...rest : any[]) {
-    if (currentLevel < level) {
+
+function parseProcessLevel(): LOG {
+    if (typeof (process.env['LOG_LEVEL']) !== "undefined") {
+        return parseInt(<string>process.env.LOG_LEVEL, 10);
+    }
+    return LOG.INFO;
+}
+
+export function debug(level: number, ...rest: any[]) {
+    if (LOG_RUNTIME_LEVEL < level) {
         return;
     }
     return _debug(...rest);
 }
 
-
 export function parseXml(xml: string) {
     const options = {
-         explicitArray: false,
+        explicitArray: false,
     };
     return new Promise((resolve, reject) => {
         xml2js.parseString(xml, options, (err, parsed) => {
@@ -38,8 +46,7 @@ export function parseXml(xml: string) {
     })
 }
 
-
-export function signMessage(message: string, cert: string, privateKey: string) {
+export function signMessage(message: string, cert: string, privateKey: string): string {
     const p7 = (<any>forge).pkcs7.createSignedData();
     p7.content = forge.util.createBuffer(message, 'utf8');
     p7.addCertificate(cert);
@@ -61,6 +68,3 @@ export function signMessage(message: string, cert: string, privateKey: string) {
     const bytes = forge.asn1.toDer(p7.toAsn1()).getBytes();
     return Buffer.from(bytes, 'binary').toString('base64');
 }
-
-
-
